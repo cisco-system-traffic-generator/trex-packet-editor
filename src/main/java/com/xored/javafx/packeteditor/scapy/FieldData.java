17845a0edaf20d6c17752b5645e0a21f2d838fcf
Anton Kiselev <anton.kisel@gmail.com>
2016-09-24 20:16:15 +0700

    add value_base64

--- src/main/java/com/xored/javafx/packeteditor/scapy/FieldData.java
+++ src/main/java/com/xored/javafx/packeteditor/scapy/FieldData.java
@@ -2,10 +2,13 @@ package com.xored.javafx.packeteditor.scapy;
 
 import com.google.gson.JsonElement;
 
+import java.util.Base64;
+
 public class FieldData {
     public String id;
     public JsonElement value;
     public String hvalue;
+    public String value_base64;
     public Number offset;
     public Number length;
 
@@ -14,4 +17,8 @@ public class FieldData {
 
     public int getLength() { return length.intValue(); }
     public int getOffset() { return offset.intValue(); }
+
+    /** if value can not be passed as JsonElement, it is passed as a value_base64 */
+    public boolean hasBinaryData() { return value_base64 != null; }
+    public byte[] getBinaryData() { return hasBinaryData() ? Base64.getDecoder().decode(value_base64) : null; }
 }
