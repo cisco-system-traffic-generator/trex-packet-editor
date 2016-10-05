8840325e20c1c766c465f77b7b1abf0e5633f361
Anton Kiselev <anton.kisel@gmail.com>
2016-10-05 19:29:05 +0700

    add ci-test script

---
+++ scripts/ci-test.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -x -e
+
+# test script for continuous integration
+
+echo "Downloading scapy server, w/o running"
+PYTHON=true ./scripts/run_scapy_server > /dev/null
+
+start-stop-daemon --start --quiet --pidfile /var/run/scapy-server.pid -b --exec $PWD/scripts/run_scapy_server
+
+./gradlew --no-daemon -Pheadless -Pjdk31 jar test intTest
+
