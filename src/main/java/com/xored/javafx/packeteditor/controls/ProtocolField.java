c60b1873b038eb5a86125cff11fa0c5c8ed4bf16
Igor Chebykin <igor.chebykin@xored.com>
2016-10-31 19:30:11 +0700

    forget invalid value on focus lost

--- src/main/java/com/xored/javafx/packeteditor/controls/ProtocolField.java
+++ src/main/java/com/xored/javafx/packeteditor/controls/ProtocolField.java
@@ -321,12 +321,8 @@ public class ProtocolField extends FlowPane {
         tf.focusedProperty().addListener((observable, oldValue, newValue) -> {
             // On lost focus
             if (!newValue) {
-                if(textChanged) {
-                    if (isValid()) {
-                        commitChanges(tf);
-                    } else {
-                        tf.requestFocus();
-                    }
+                if(textChanged && isValid()) {
+                    commitChanges(tf);
                 } else {
                     showLabel();
                 }
