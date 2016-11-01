58c1d9e95cb71a2911d52bf20fea218a3c83d449
Anton Kiselev <anton.kisel@gmail.com>
2016-11-01 15:42:31 +0700

    don't use protocols.json

--- src/main/java/com/xored/javafx/packeteditor/scapy/ScapyDefinitions.java
+++ src/main/java/com/xored/javafx/packeteditor/scapy/ScapyDefinitions.java
@@ -1,11 +1,14 @@
 package com.xored.javafx.packeteditor.scapy;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
 import java.util.List;
 
 /**
  * structure of scapy.get_definitions
+ * see https://github.com/cisco-system-traffic-generator/trex-doc/blob/master/trex_scapy_rpc_server.asciidoc
  */
 public class ScapyDefinitions {
     public class ScapyProtocol {
@@ -17,8 +20,13 @@ public class ScapyDefinitions {
     public class ScapyField {
         public String id;
         public String name;
-        public String field_type;
-        public JsonElement values_dict;
+        public String type;
+        public Boolean auto;
+        public Integer min;
+        public Integer max;
+        public String regex;
+        public JsonObject values_dict;
+        public JsonArray bits;
     }
 
     public List<ScapyProtocol> protocols;
