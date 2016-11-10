eb636152c7ead7f3354b6f164e7d03397d5a6551
Vyacheslav Ogai <vyacheslav.ogai@gmail.com>
2016-11-11 03:38:25 +0700

    Remove unused code.

--- src/main/java/com/xored/javafx/packeteditor/data/user/FEInstruction.java
+++ src/main/java/com/xored/javafx/packeteditor/data/user/FEInstruction.java
@@ -1,7 +1,6 @@
 package com.xored.javafx.packeteditor.data.user;
 
 import java.util.Map;
-import java.util.stream.Collectors;
 
 public class FEInstruction {
     // ProtocolId.FieldId
@@ -37,14 +36,4 @@ public class FEInstruction {
         parameters.put(parameterId, value);
     }
 
-    private String getVarName() {
-        return id.replace(".", "_");
-    }
-    
-    public String getVmFlowVar() {
-        return "STLVmFlowVar(name=\""+getVarName()+"\"," + parameters.entrySet().stream().map(entry -> entry.getKey() + "=\"" + entry.getValue()+"\"").collect(Collectors.joining(",")) + ")";
-    }
-    public String getVmWrFlowVar() {
-        return "STLVmWrFlowVar(fv_name=\""+getVarName()+"\",pkt_offset=\""+id+"\")";
-    }
 }
