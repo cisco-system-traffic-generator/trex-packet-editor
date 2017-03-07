78f1a0eb1cd4633cf6411d1ef34975c3f0057576
Anton Kiselev <anton.kisel@gmail.com>
2017-03-07 17:50:42 +0700

    run trex-core tests on CI. temporary switch to kisel/trex-core

--- scripts/ci-test.sh
+++ scripts/ci-test.sh
@@ -5,6 +5,9 @@
 echo "Downloading scapy server, w/o running"
 DOWNLOAD_ONLY=true ./scripts/run_scapy_server
 
+echo "Running scapy server tests"
+RUN_SCAPY_TESTS_ONLY=true ./scripts/run_scapy_server
+
 start-stop-daemon --start --quiet --pidfile /var/run/scapy-server.pid -b --exec $PWD/scripts/run_scapy_server
 
 ./gradlew --no-daemon -Pheadless jar test intTest $@
